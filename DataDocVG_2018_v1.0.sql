db999e62ac578e61e0ec4274ad126463394917f4
Giancarlo Valencia Sotomayor <gvalencia@vallegrande.edu.pe>
2019-10-04 09:20:56 -0500

    Add files via upload

---
+++ DataDocVG_2018_v1.0.sql
@@ -0,0 +1,39 @@
+
+insert into persona values
+--NOMPER             APEPER            CELPER               CORPER                    NIPER   TIPPER ESTPER DIRPER     CODUBI NACPER     TITPER
+('CAMPOS CASTILLO', 'FLAVIO ALEXANDER','95474577','flavio.campos@vallegrande.edu.pe ','74974209','A','A','urb. Libertad','150301','07/25/2002','Sr.'),
+('CORREA PALOMINO', 'JOSE ANDRES','95474577','jose.correa@vallegrande.edu.pe','71741757','A','A','urb. Libertad','150301','12/11/1996','Sr.'),
+('CUYA VICENTE', 'ZECARLOS GUILLERMO','95474577','zecarlos.cuya@vallegrande.edu.pe','70191277','A','A','urb. Libertad','150301','10/11/2001','Sr.'),
+('DIAZ MEDRANO', 'BENJAMIN ARTURO','95474577','benjamin.diaz@vallegrande.edu.pe','73769363','A','A','urb. Libertad','150301','08/01/2002','Sr.'),
+('FLORIAN FLORES',' JOSEPH JESUS','95475366','jflorianf@vallegrande.edu.pe ','75365805','A','A','urb. Libertad','150301','02/09/1999','Sr.'),
+('HUAMAN TIRADO',' LUCIO CARLOS','95474577','lucio.huaman@vallegrande.edu.pe ','70277538','A','A','urb. Libertad','150301','09/02/2002','Sr.'),
+('HUAPAYA RIVERA',' DIEGO','95474577','diego.huapaya@vallegrande.edu.pe ','72036321','A','A','urb. Libertad','150301','04/04/2002','Sr.'),
+('MEZA YALLE',' WILMER NOE','95474577','wilmer.meza@vallegrande.edu.pe','75396928','A','A','urb. Libertad','150301','11/05/2001','Sr.'),
+('PALOMINO FLORES',' JEFERSON JESUS','95474577','jeferson.palomino@vallegrande.edu.pe ','72530657','A','A','urb. Libertad','150301','01/07/2002','Sr.'),
+('PALOMINO VICENTE',' JEAN POOL ALBERT','95474577','jean.palomino@vallegrande.edu.pe ','73016794','A','A','urb. Libertad','150301','08/06/2002','Sr.'),
+('QUISPE CHAVEZ',' JULIO CESAR','95474577','julio.quispe@vallegrande.edu.pe ','77077399','A','A','urb. Libertad','150301','09/10/2001','Sr.'),
+('RIVERA YARIHUAMAN',' JOSE LUIS','95474577','jose.rivera@vallegrande.edu.pe ','75107263','A','A','urb. Libertad','150301','07/11/2000','Sr.'),
+('SALAS DIAZ',' JOSEPH LIZARDO','95474577','joseph.salas@vallegrande.edu.pe ','70248502','A','A','urb. Libertad','150301','12/04/1999','Sr.'),
+('SAMANIEGO TORREBLANCA',' SEBASTIAN MANUEL','95474577','sebastian.samaniego@vallegrande.edu.pe ','70144473','A','A','urb. Libertad','150301','05/09/2001','Sr.'),
+('SANCHEZ GAMEZ',' JESUS ARTURO','95474577','jesus.sanchez@vallegrande.edu.pe ','73496872','A','A','urb. Libertad','150301','09/12/2000','Sr.'),
+('SANDOVAL ROSALES',' VICTOR GEOVANI','95474577','victor.sandoval@vallegrande.edu.pe ','60323413','A','A','urb. Libertad','150301','07/07/2002','Sr.'),
+('SIMON ROJAS',' JEAN CARLOS','95474577','jean.simon@vallegrande.edu.pe ','76093168','A','A','urb. Libertad','150301','04/03/2001','Sr.'),
+('TAQUIRE MANUEL',' LUIS ANGEL','95474577','luis.taquire@vallegrande.edu.pe ','74177853','A','A','urb. Libertad','150301','07/03/1998','Sr.'),
+('YAMANI LIZANA',' JULIO JESUS','95474577','julio.yamani@vallegrande.edu.pe ','70785722','A','A','urb. Libertad','150301','03/07/1998','Sr.')
+
+
+go
+insert into empresa values
+-- RAZEMP COMEMP  RUCEMP ESTEMP DIREMP  CODUBI TELEMP  
+('Comercial Importadora Jean Paul EIRL','Claro','20491265731','A','Jr. Dos de Mayo Nº S/N Interior 2-A ','150102','5812381'),
+('Hospital Rezola','Hospital Rezola','20491265732','A',' Calle San Martín Nº 124','150103','5812382'),
+('Unidad Ejecutora Lima Sur - Región Lima','Unidad Ejecutora Lima Sur - Región Lima','20491265733','A','Carretera Panamericana Sur Km. 144.5','150104','5812383'),
+('Municipalidad Distrital de Quilmaná','Municipalidad Distrital de Quilmaná','20491265734','A','Av. Lima Nº 451','150105','5812384'),
+('Servicios Huarco S.A.','Servicios Huarco S.A.','20491265735','A','Pasaje Los Olivos S/N ','150106','5812385'),
+('Institución Educativa de Acción Conjunta Apóstol San Pedro','Institución Educativa de Acción Conjunta Apóstol San Pedro','20491265736','A','Carretera a Quilmaná km 0.50','150107','5812386'),
+('SBS Cañete - Yauyos','SBS Cañete - Yauyos','20491265737','A','Carretera Panamericana Sur Km. 144.5','150108','5812387'),
+('Cáritas Cañete - Yauyos','Cáritas','20491265738','A','Urb. Primavera Mz F lote 1A ','150109','5812388'),
+('Notaria Garrafa','Notaria Garrafa','20491265739','A','Jr. O’Higgins Nº 228','150110','5812389'),
+('IEP San Josemaría','IEP San Josemaría','20491265740','A','Calle Azahares Nº 178','150111','5812390'),
+('GRUCORPERU S.A.C.','GRUCORPERU S.A.C.','20491265741','A','Jr. El Carmen Nro. 324','150112','5812391'),
+('Institución Educativa Parroquial San José','Institución Educativa Parroquial San José','20491265742','A','Car. Imperial a Quilmaná Km. 5','150113','5812392')
